c3aac661e0524c4262135572acf46787a64c88e0
LouayMagdy <magdylouay@gmail.com>
2022-12-28 13:35:06 +0200

    Cart, CartBook, Sale models added

---
+++ demo/src/main/java/com/example/demo/model/Cart.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    int cartID;
+    String userName;
+    double totalPrice;
+    CartState state;
+}
