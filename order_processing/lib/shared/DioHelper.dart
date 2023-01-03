8793dc7193186a183bafdf8616beaaefc037e0bd
AbdelrahmanMosly <abdelrahman.b.mosly@gmail.com>
2023-01-03 21:56:33 +0200

    change to local host

--- order_processing/lib/shared/DioHelper.dart
+++ order_processing/lib/shared/DioHelper.dart
@@ -7,7 +7,7 @@ class DioHelper {
   {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'http://192.168.1.2:8080/',
+        baseUrl: 'http://localhost:8080/',
         receiveDataWhenStatusError: true,
       ),
     );
