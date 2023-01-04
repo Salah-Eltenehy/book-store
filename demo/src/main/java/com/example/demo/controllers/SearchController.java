2d613fecdb05c63e938daead60f1e79929fc4ebd
abdelrahman316gad <abdelrahmanfencergadafc@gmail.com>
2023-01-04 19:21:57 +0200

    Revert "edit get request search to path variable"
    This reverts commit 85154fd1d0c27e6e703155e60e81f163140a522d.

--- demo/src/main/java/com/example/demo/controllers/SearchController.java
+++ demo/src/main/java/com/example/demo/controllers/SearchController.java
@@ -7,7 +7,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -23,28 +26,28 @@ public class SearchController {
         this.searchAgent = searchAgent;
 //        System.out.println(searchAgent.dbAgent);
     }
-    @GetMapping("/isbn/{keyword}/{offset}")
-    ResponseEntity<String> searchByISBN(@PathVariable String keyword, @PathVariable Integer offset) throws SQLException {
+    @GetMapping("/isbn")
+    ResponseEntity<String> searchByISBN(@RequestParam String keyword, @RequestParam Integer offset) throws SQLException {
         return new ResponseEntity<>(new Gson().toJson(searchAgent.searchByISBN(keyword, offset)), HttpStatus.OK);
     }
-    @GetMapping("/title/{keyword}/{offset}")
-    ResponseEntity<String> searchByTitle(@PathVariable String keyword, @PathVariable Integer offset) throws SQLException {
+    @GetMapping("/title")
+    ResponseEntity<String> searchByTitle(@RequestParam String keyword, @RequestParam Integer offset) throws SQLException {
         return new ResponseEntity<>(new Gson().toJson(searchAgent.searchByTitle(keyword, offset)), HttpStatus.OK);
     }
-    @GetMapping("/category/{keyword}/{offset}")
-    ResponseEntity<String> searchByCategory(@PathVariable String keyword, @PathVariable Integer offset) throws SQLException {
+    @GetMapping("/category")
+    ResponseEntity<String> searchByCategory(@RequestParam String keyword, @RequestParam Integer offset) throws SQLException {
         return new ResponseEntity<>(new Gson().toJson(searchAgent.searchByCategory(keyword, offset)), HttpStatus.OK);
     }
-    @GetMapping("/publisher/{keyword}/{offset}")
-    ResponseEntity<String> searchByPublisher(@PathVariable String keyword, @PathVariable Integer offset) throws SQLException {
+    @GetMapping("/publisher")
+    ResponseEntity<String> searchByPublisher(@RequestParam String keyword, @RequestParam Integer offset) throws SQLException {
         return new ResponseEntity<>(new Gson().toJson(searchAgent.searchByPublisher(keyword, offset)), HttpStatus.OK);
     }
-    @GetMapping("/author/{keyword}/{offset}")
-    ResponseEntity<String> searchByAuthor(@PathVariable String keyword, @PathVariable Integer offset) throws SQLException {
+    @GetMapping("/author")
+    ResponseEntity<String> searchByAuthor(@RequestParam String keyword, @RequestParam Integer offset) throws SQLException {
         return new ResponseEntity<>(new Gson().toJson(searchAgent.searchByAuthor(keyword, offset)), HttpStatus.OK);
     }
-    @GetMapping("/all/{offset}")
-    ResponseEntity<String> getAllBooks(@PathVariable Integer offset) throws SQLException {
+    @GetMapping("/all")
+    ResponseEntity<String> getAllBooks(@RequestParam Integer offset) throws SQLException {
 //        System.out.println("all");
         List<Book> books = searchAgent.getAllBooks(offset);
         System.out.println(books.toString());
